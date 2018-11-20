4517b6fb55fb4e4ee6f951a6d910d0c3e28e8f4d
Jim Weber <jpweber@gmail.com>
2018-11-20 12:58:39 -0500

    Created ability to have concurrent file downloads
    Signed-off-by: Jim Weber <jpweber@gmail.com>

--- getobjects.go
+++ getobjects.go
@@ -1,24 +1,39 @@
 package main
 
 import (
-	"context"
 	"log"
-	"time"
+	"sync"
 
 	minio "github.com/minio/minio-go"
 )
 
 func getFiles(client *minio.Client, filesToDownload []string, bucketName, dest string) {
-	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
-	defer cancel()
+
+	concurrency := 2
+	workerPool := make(chan bool, concurrency)
+	for i := 0; i < concurrency; i++ {
+		workerPool <- true
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(len(filesToDownload))
 
 	for _, fileName := range filesToDownload {
-		// DEBUG
-		log.Println("downloading", fileName)
-		err := client.FGetObjectWithContext(ctx, bucketName, fileName, dest+"/"+fileName, minio.GetObjectOptions{})
-		if err != nil {
-			log.Println("Error downloading file ", err)
-			return
-		}
+		<-workerPool
+		go func(wg *sync.WaitGroup, fileName string) {
+			log.Println("downloading", fileName)
+			// Spin this out in to go routines so we can download concurrently.
+			// might want to buffer this with channels though so we don't saturate things
+			err := client.FGetObject(bucketName, fileName, dest+"/"+fileName, minio.GetObjectOptions{})
+			if err != nil {
+				log.Println("Error downloading file ", err)
+				return
+			}
+
+			workerPool <- true
+			wg.Done()
+		}(&wg, fileName)
+
 	}
+	wg.Wait()
 }
