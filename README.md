36974823e9f7e3e4ad84bb6b3cb87ab89c94c460
Jim Weber <jpweber@gmail.com>
2018-11-23 17:49:35 -0500

    cleanup of deadcode and readme linting fixes
    Signed-off-by: Jim Weber <jpweber@gmail.com>

--- README.md
+++ README.md
@@ -1,28 +1,26 @@
 # Minic
 
-Download data from minio via an init container. Useful pre-loading volumes for stateful applications, or anything that needs data before the applications starts. 
-
+Download data from minio via an init container. Useful pre-loading volumes for stateful applications, or anything that needs data before the applications starts.
 
 ## Parameters
+
 Parameters are read from the following environment variables  
+
 - `MINIO_URL` = address and port for the minio server. Does not need scheme.  
 - `ACCESSKEY` = Minio access key  
 - `SECRETKEY` = Minio secret key  
-- `SRC` = Source of the files you wish to copy to your container.  
-Takes form of `bucketname/path/file` or `bucketname/path` for all contents in a folder  
+- `SRC` = Source of the files you wish to copy to your container. Takes form of `bucketname/path/file` or `bucketname/path` for all contents in a folder  
 - `DEST` = Path to where you wish data to inside your container  
 
-Special note for the `DEST` paramert. If you are downloading all files from a dir, don't include that dir name in your `DEST` Value. For example. If you were downloading all the contents from `users/roles` do _not_ make your destination `/data/users/roles`. *Do* make it `/data/users`. The roles directory will be created as part of the download. 
-
-
+Special note for the `DEST` paramert. If you are downloading all files from a dir, don't include that dir name in your `DEST` Value. For example. If you were downloading all the contents from `users/roles` do _not_ make your destination `/data/users/roles`. *Do* make it `/data/users`. The roles directory will be created as part of the download.
 
 ## Using it as init container in Kubernetes
 
-Include this in your current deployment. Modify the `env` values to match your environment.  The name of the container can be changed to whatever you wish. Below example asssumes you already have a mount named `db-storage` in your main container. This should match where your primary container is going to read its data. 
+Include this in your current deployment. Modify the `env` values to match your environment.  The name of the container can be changed to whatever you wish. Below example asssumes you already have a mount named `db-storage` in your main container. This should match where your primary container is going to read its data.
 
 ``` yaml
  initContainers:
-  - name: data-loader 
+  - name: data-loader
     image: jpweber/minio-init-dl
     env:
     - name: MINIO_URL
@@ -46,7 +44,9 @@ Create a secret to hold your access and secret keys
 ## Run locally for testing
 
 ### Binary CLI
+
 Set your variables like the example below
+
 ``` shell
 export MINIO_URL="192.168.2.66:9000"
 export SRC="vectorizer/roles"
@@ -54,6 +54,7 @@ export ACCESSKEY="LEB3JJ3OCBN4HTDIS5IZ"
 export SECRETKEY="xs3fx83cMkV7Oh+6jlGGTt9kTmT5D6yoQLm9+L5X"
 export DEST="/tmp/minio-tests/examples"
 ```
+
 Then you can run the binary with out any extra parameters
 
 ### Docker Container
--- objects.go
+++ objects.go
@@ -2,137 +2,65 @@ package main
 
 import (
 	"crypto/md5"
-	"encoding/hex"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
-	"strconv"
-	"strings"
 	"sync"
 
 	minio "github.com/minio/minio-go"
 )
 
-func fileOpener(filePath string) (*os.File, error) {
-	//Open the passed argument and check for any error
-	file, err := os.Open(filePath)
-	if err != nil {
-		return nil, err
+func getEtag(path string, partSizeMb int) string {
+	partSize := partSizeMb * 1024 * 1024
+	content, _ := ioutil.ReadFile(path)
+	fileSize := len(content)
+	contentToHash := content
+	parts := 0
+
+	if fileSize > partSize {
+		pos := 0
+		contentToHash = make([]byte, 0)
+		for fileSize > pos {
+			endpos := pos + partSize
+			if endpos >= fileSize {
+				endpos = fileSize
+			}
+			hash := md5.Sum(content[pos:endpos])
+			contentToHash = append(contentToHash, hash[:]...)
+			pos += partSize
+			parts++
+		}
 	}
 
-	//Tell the program to call the following function when the current function returns
-	// 	defer file.Close()
-	// we can't actually defer close here we need to hold it open and close it later when we are done chunking
-	return file, nil
-}
-
-func chunker(chunkSize, offSet int64, file *os.File) ([]byte, error) {
-	// create byte array to store our chunk of bytes
-	byteChunk := make([]byte, chunkSize)
-
-	// seek to the correct position of the file
-	// When needing to hash mult-part files each part is hashed on its own
-	// we have to seek to the right part of the file to read in just those bytes
-	// as if we were uploading the file
-	_, err := file.Seek(offSet, 0)
-	if err != nil {
-		return nil, err
+	hash := md5.Sum(contentToHash)
+	etag := fmt.Sprintf("%x", hash)
+	if parts > 0 {
+		etag += fmt.Sprintf("-%d", parts)
 	}
-
-	// read in our chunk of data
-	_, err = file.Read(byteChunk)
-	if err != nil {
-		return nil, err
-	}
-
-	return byteChunk, nil
-}
-
-func hasher(dataChunk []byte) string {
-
-	//Initialize variable returnMD5String now in case an error has to be returned
-	var md5String string
-
-	//Open a new hash interface to write to
-	hash := md5.New()
-
-	//Get the 16 bytes hash
-	hashInBytes := hash.Sum(dataChunk)[:16]
-
-	//Convert the bytes to a string
-	md5String = hex.EncodeToString(hashInBytes)
-
-	return md5String
-
+	return etag
 }
 
 // checksumMatch - returns true if they match, false if they do not match
-func checksumMatch(remoteChecksum, localFilePath string, numParts int) bool {
+func checksumMatch(remoteChecksum, localFilePath string) bool {
 
 	// check if local file exists. If not return false right away
 	_, err := os.Stat(localFilePath)
 	if err != nil {
-		log.Println("Could not find file", localFilePath, "locally to checksum")
+		log.Println("Could not find file", localFilePath, "Skipping checksum and downloading")
 		return false
 	}
 
 	// get md5sum of local file
-	file, err := fileOpener(localFilePath)
-	if err != nil {
-		log.Println("Error opening file", err)
-		return false
-	}
-	defer file.Close()
-
-	// init var to pass to hasher
-	localBytes := []byte{}
-	if numParts == 1 {
-		//the hash is of the single file, not the combined chunks
-		fileInfo, err := file.Stat()
-		if err != nil {
-			log.Println("Error stating local file", localFilePath, err)
-			return false
-		}
-		localBytes, err = chunker(fileInfo.Size(), 0, file)
-		if err != nil {
-			return false
-		}
-
-	} else {
-		// we are dealing with a hash that is comprised of multiple chunks
-		chunkHashes := []string{}
-
-		// assuming 64mb file size for now
-		chunkSize := 64000
-		offSet := 0
-		for i := 0; i < numParts; i++ {
-			chunkBytes, err := chunker(int64(chunkSize), int64(offSet), file)
-			if err != nil {
-				log.Println("Error getting bytes for chunk", err)
-				return false
-			}
-			chunkHash := hasher(chunkBytes)
-			chunkHashes = append(chunkHashes, chunkHash)
-			offSet += chunkSize
-		}
-
-		// DEBUG
-		fmt.Println(chunkHashes)
-		combinedChunkHashes := strings.Join(chunkHashes, "")
-		localBytes = []byte(combinedChunkHashes)
-
-	}
-
-	localChecksum := hasher(localBytes)
+	localChecksum := getEtag(localFilePath, 64)
 	if err != nil {
 		log.Println("Error getting local md5 checksum", err)
 		return false
 	}
 
-	// DEBUG
 	log.Println("Local Sum:", localChecksum, "Remote Sum:", remoteChecksum)
 	// compare local md5 with remote md5
-	if localChecksum == remoteChecksum {
+	if string(localChecksum) == remoteChecksum {
 		return true
 	}
 	// return false by default
@@ -153,27 +81,16 @@ func getFiles(client *minio.Client, filesToDownload []string, bucketName, dest s
 
 	for _, fileName := range filesToDownload {
 		<-workerPool
-		// DEBUG
-		// log.Println("Stat", bucketName, fileName)
 		go func(wg *sync.WaitGroup, fileName string) {
 			stat, err := client.StatObject(bucketName, fileName, minio.StatObjectOptions{})
 			if err != nil {
 				log.Fatalln(err)
 			}
 
-			// read type from stat object to see if it is an octet stream
-			// if it is we need to do the multi part hashing. The minio Etag format
-			// is hash-numberofparts. Figure out the number of parts for use later
-			multiPartCount := 1
-			if stat.ContentType == "application/octet-stream" {
-				ETagCount, _ := strconv.Atoi(strings.Split(stat.ETag, "-")[1])
-				multiPartCount = ETagCount
-			}
-
 			// if the checksums match we don't need to download because they are
 			// the same thing
-			log.Println("Etag from", bucketName, fileName)
-			if checksumMatch(stat.ETag, dest+"/"+fileName, multiPartCount) {
+			if checksumMatch(stat.ETag, dest+"/"+fileName) {
+				log.Printf("%s/%s Has Not Changed. Not downloading.", dest, fileName)
 				workerPool <- true
 				wg.Done()
 				return
