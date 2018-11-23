de7f69ded47b6609de3d9b2e1e680f7f1f724e25
Jim Weber <jpweber@gmail.com>
2018-11-22 23:13:05 -0500

    Some error fixups
    Signed-off-by: Jim Weber <jpweber@gmail.com>

--- objects.go
+++ objects.go
@@ -3,7 +3,6 @@ package main
 import (
 	"crypto/md5"
 	"encoding/hex"
-	"io"
 	"log"
 	"os"
 	"sync"
@@ -19,39 +18,38 @@ func fileOpener(filePath string) (*os.File, error) {
 	}
 
 	//Tell the program to call the following function when the current function returns
-// 	defer file.Close()
+	// 	defer file.Close()
 	// we can't actually defer close here we need to hold it open and close it later when we are done chunking
-	
-}
 
+}
 
 func chunker(chunkSize, offSet int64, file *os.File) ([]byte, error) {
 	// create byte array to store our chunk of bytes
 	byteChunk := make([]byte, chunkSize)
-	
-	// seek to the correct position of the file 
+
+	// seek to the correct position of the file
 	// When needing to hash mult-part files each part is hashed on its own
-	// we have to seek to the right part of the file to read in just those bytes 
+	// we have to seek to the right part of the file to read in just those bytes
 	// as if we were uploading the file
-	_, err = file.Seek(offset, 0)
-   	if err != nil {
-        return nil, err
-    }
-		
-		// read in our chunk of data
-    byteNum, err := file.Read(byteChunk)
-    if err != nil {
-        return nil, err
-    }
-		
-		return byteChunk, nil
+	_, err := file.Seek(offSet, 0)
+	if err != nil {
+		return nil, err
+	}
+
+	// read in our chunk of data
+	byteNum, err := file.Read(byteChunk)
+	if err != nil {
+		return nil, err
+	}
+
+	return byteChunk, nil
 }
 
 func hasher(dataChunk []byte) string {
-	
+
 	//Initialize variable returnMD5String now in case an error has to be returned
 	var md5String string
-	
+
 	//Open a new hash interface to write to
 	hash := md5.New()
 
@@ -76,7 +74,8 @@ func checksumMatch(remoteChecksum, localFilePath string, multiPart bool) bool {
 	}
 
 	// get md5sum of local file
-	localChecksum, err := hasher(localFilePath)
+	// change the input to hasher to be the output from chunker
+	localChecksum := hasher(localFilePath)
 	if err != nil {
 		log.Println("Error getting local md5 checksum", err)
 		return false
@@ -111,7 +110,7 @@ func getFiles(client *minio.Client, filesToDownload []string, bucketName, dest s
 			if err != nil {
 				log.Fatalln(err)
 			}
-			
+
 			// read type from stat object to see if it is an octet stream
 			// if it is we need to do the multi part hashing
 			multiPart := false
